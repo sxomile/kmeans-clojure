1cd15e36b238a051f672076990be32c1dcd8b7e2
sxomile <milosprorocic1307@gmail.com>
2026-05-02 21:37:10 +0200

    added button that looks kinda nice

--- src/kmeans_clojure/ui.clj
+++ src/kmeans_clojure/ui.clj
@@ -1,11 +1,64 @@
 (ns kmeans-clojure.ui
-  (:import [javax.swing JFrame JLabel]))
+  (:require [kmeans-clojure.csv :as csv]
+            [kmeans-clojure.visual :as visual])
+  (:import [javax.swing JFrame JButton JLabel JPanel BoxLayout Box BorderFactory]
+           [java.awt Dimension Color Font]))
 
 (defn create-ui []
   (let [frame (JFrame. "K-Means App")
-        label (JLabel. "Hello K-Means")]
+        root (JPanel.)                                      ; outer panel (background)
 
-    (.add frame label)
-    (.setSize frame 300 200)
+        panel (JPanel.)                                     ;inner panel (card)
+
+        load-btn (JButton. "Load CSV")
+        visualize-btn (JButton. "Visualize")
+        info-label (JLabel. "No dataset loaded")]
+
+    ; ===== ROOT PANEL (background) =====
+    (.setBackground root (Color. 240 242 245))
+    (.setLayout root (BoxLayout. root BoxLayout/Y_AXIS))
+
+    ; ===== INNER PANEL (card style) =====
+    (.setLayout panel (BoxLayout. panel BoxLayout/Y_AXIS))
+    (.setBackground panel Color/WHITE)
+    (.setBorder panel (BorderFactory/createEmptyBorder 20 30 20 30))
+
+    ; ===== FONTS =====
+    (.setFont info-label (Font. "Arial" Font/PLAIN 14))
+
+    ; ===== BUTTON STYLE =====
+    (doseq [btn [load-btn visualize-btn]]
+      (.setFocusPainted btn false)
+      (.setFont btn (Font. "Arial" Font/BOLD 14))
+      (.setBackground btn (Color. 66 133 244)) ; blue
+      (.setForeground btn Color/WHITE)
+      (.setMaximumSize btn (Dimension. 200 40))
+      (.setAlignmentX btn 0.5))
+
+    ; label center
+    (.setAlignmentX info-label 0.5)
+
+    ; hide visualize initially
+    (.setVisible visualize-btn false)
+
+    ; ===== LAYOUT =====
+    (.add panel (Box/createRigidArea (Dimension. 0 10)))
+    (.add panel load-btn)
+
+    (.add panel (Box/createRigidArea (Dimension. 0 20)))
+    (.add panel info-label)
+
+    (.add panel (Box/createRigidArea (Dimension. 0 20)))
+    (.add panel visualize-btn)
+
+    ; center panel inside root
+    (.add root (Box/createVerticalGlue))
+    (.add root panel)
+    (.add root (Box/createVerticalGlue))
+
+    ; ===== FRAME =====
+    (.add frame root)
+    (.setSize frame 500 350)
+    (.setLocationRelativeTo frame nil)
     (.setDefaultCloseOperation frame JFrame/EXIT_ON_CLOSE)
     (.setVisible frame true)))
